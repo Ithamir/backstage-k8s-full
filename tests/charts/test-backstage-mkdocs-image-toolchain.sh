40836ed97b7b335b8dad3609b33c4e4e955c1104
Itamar Ratson <itamar.ratson@gmail.com>
2026-05-18 22:52:43 +0000

    Refine Backstage MkDocs image toolchain placement

--- tests/charts/test-backstage-mkdocs-image-toolchain.sh
+++ tests/charts/test-backstage-mkdocs-image-toolchain.sh
@@ -5,13 +5,13 @@ source "$(dirname "$0")/helpers.sh"
 
 echo "=== Backstage mkdocs image toolchain tests ==="
 
-dockerfile=$(cat backstage/Dockerfile)
 dockerignore=$(cat backstage/.dockerignore)
+final_stage=$(awk '/^FROM node:22-bookworm-slim$/ { in_final_stage = 1; next } in_final_stage { print }' backstage/Dockerfile)
 
-assert_contains "final image installs python3-pip" "$dockerfile" "python3-pip"
-assert_contains "techdocs core is pinned" "$dockerfile" "mkdocs-techdocs-core==1.6.2"
-assert_contains "mermaid plugin is pinned" "$dockerfile" "mkdocs-mermaid2-plugin==1.2.3"
-assert_contains "pip bypasses externally managed marker" "$dockerfile" "--break-system-packages"
+assert_contains "final image installs python3-pip" "$final_stage" "python3-pip"
+assert_contains "techdocs core is pinned" "$final_stage" "mkdocs-techdocs-core==1.6.2"
+assert_contains "mermaid plugin is pinned" "$final_stage" "mkdocs-mermaid2-plugin==1.2.3"
+assert_contains "pip bypasses externally managed marker" "$final_stage" "--break-system-packages"
 assert_contains "dockerignore excludes docs" "$dockerignore" "docs/"
 
 report_results "Backstage mkdocs image toolchain"
