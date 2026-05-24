10519bb655caf75f84d55fdcd89eabde2812f99f
Itamar Ratson <itamar.ratson@gmail.com>
2026-05-24 23:46:31 +0300

    Revert "Refine LoadBalancer exposure configuration"
    This reverts commit 870f1a05ab161bdbfe5b399e1ec45089e59d651a.

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,8 +1,6 @@
 locals {
-  root_application = yamldecode(file("${path.module}/bootstrap/root-app.yaml"))
-
-  generated_dir             = abspath("${path.module}/.generated")
-  nginx_lb_proxy_config     = "${local.generated_dir}/nginx-lb-proxy.conf"
+  root_application          = yamldecode(file("${path.module}/bootstrap/root-app.yaml"))
+  nginx_lb_proxy_config     = abspath("${path.module}/.generated/nginx-lb-proxy.conf")
   nginx_lb_proxy_config_raw = <<-EOT
     server {
       listen 80;
